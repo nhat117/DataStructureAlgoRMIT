5b750102950a3977f61170726c109fb37483c942
nhat1 <nhat117>
2021-11-28 17:23:51 +0700

    Commit

---
+++ src/W6/W6.java
@@ -0,0 +1,164 @@
+package W6;
+
+import javax.management.StandardEmitterMBean;
+import java.util.SortedMap;
+
+public class W6 {
+    public static void main(String[] args) {
+        //Problem 1
+        W6Tree tree = new W6Tree();
+        for(int i = 0; i < 10; i ++) {
+            tree.insertBST((int)(Math.random() * 100));
+        }
+
+        //Problem 2
+        tree.startPreorder();
+        tree.startInOrder();
+        tree.startPostOrder();
+        tree.breadthFirst();
+        //Problem 3
+        System.out.println("Tree Height" + tree.treeHeight());
+    }
+
+    static class W6Node{
+        public Object value;
+        public W6Node l, r;
+        //A typical tree node
+        public W6Node(Object value) {
+            this.value = value;
+        }
+    }
+    static  class  W6Tree {
+        public W6Node root;
+        //Usefull in inorder traversal
+        public int lastValue;
+        public boolean isIncrease;
+
+        //For Display
+        int row = 10;
+        int cols = 80;
+        char [][] canvas = new char[row][cols];
+
+        public W6Tree() {
+            root = null;
+            for (int i = 0; i < row; i ++) {
+                for(int j = 0; j < cols; j ++) {
+                    canvas[i][j] = ' ';
+                }
+            }
+        }
+
+
+        //Problem 1
+        public void insertBST(Integer v) {
+            W6Node tmp = new W6Node(v);
+            int step = 1;
+            if(this.root == null) {
+                this.root = tmp;
+                System.out.printf("Insert: %d, number of steps: %d \n",v, step);
+                return;
+            }
+
+            //Find the suitable location
+            W6Node tmp2 = root;
+            while (true) {
+                //Abort if duplicated
+                if(tmp2.value == v) {
+                    System.out.printf("Insert: %d, number of steps: %d\n",v,step);
+                    return;
+                }
+                step ++;
+                if((Integer)tmp2.value > v) {
+                    if(tmp2.l == null) {
+                        tmp2.l = tmp;
+                        break;
+                    } else {
+                        tmp2 = tmp2.l;
+                    }
+                } else {
+                    if(tmp2.r == null) {
+                        tmp2.r = tmp;
+                        break;
+                    } else {
+                        tmp2 = tmp2.r;
+                    }
+                }
+            }
+            System.out.printf("Insert: %d, number of step: %d \n", v, step);
+        }
+
+        //Problem 2
+        //Preorder Traversal
+        public void startPreorder() {
+            System.out.println("Preorder Traversal");
+            preOrder(root);
+        }
+
+        private void preOrder(W6Node n) {
+            if(n == null) {
+                return;
+            }
+            System.out.println(n.value);
+            preOrder(n.l);
+            preOrder(n.r);
+        }
+        //InOrder Traversal
+        public void startInOrder() {
+            System.out.println("InOrder traversal");
+            inOrder(root);
+        }
+
+        public void inOrder(W6Node n) {
+            if( n == null) return;
+            inOrder(n.l);
+            System.out.println(n.value);
+            inOrder(n.r);
+        }
+
+        public void startPostOrder() {
+            System.out.println("Post-order traversal");
+            postOrder(root);
+        }
+        //Use to delete the tree
+        public void postOrder(W6Node n) {
+            if(n == null) return;
+            postOrder(n.l);
+            postOrder(n.r);
+            System.out.println((int)n.value);
+        }
+         public void breadthFirst() {
+             System.out.println("Breadth first");
+             //Queue
+             W6Node [] q = new W6Node[100];
+             int h, t;
+             h = t = 0;
+             //EndQ
+             q[t] = root;
+             t ++;
+             while( h < t) {
+                 W6Node tmp = q[h];
+                 h ++;
+                 System.out.println(tmp.value);
+                 if(tmp.l != null) {
+                     q[t] = tmp.l;
+                     t ++;
+                 }
+                 if(tmp.r != null) {
+                     q[t] = tmp.r;
+                     t ++;
+                 }
+             }
+          }
+
+        // Problem 3
+        public int treeHeight() {
+            return  nodeHeight(root);
+        }
+
+        private int nodeHeight(W6Node n) {
+            if(n == null) return 0;
+
+        return Math.max(nodeHeight(n.l), nodeHeight(n.r) + 1);
+        }
+    }
+}
